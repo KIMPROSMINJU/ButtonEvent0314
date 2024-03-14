f8ccaf3bbac99e63ee0716c43cd448f5f8369ee2
kimprosminju <b.m1nj00371@gmail.com>
2024-03-14 16:23:08 +0900

    4개의 Button EventListner When 활용

--- app/src/main/java/kr/ac/kopo/m1nj00/buttonevent0314/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/m1nj00/buttonevent0314/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var btn911 : Button
     lateinit var btnGallery : Button
     lateinit var btnFinish : Button
-//    var mintent = ""
+    lateinit var btnListner : View.OnClickListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,25 +26,31 @@ class MainActivity : AppCompatActivity() {
         btnGallery = findViewById(R.id.buttonGallery)
         btnFinish = findViewById(R.id.buttonFinish)
 
-//        when(btn){
-//            btn_2 -> mintent = Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"))
-//            btn_3 -> mintent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"))
-//            btn_4 -> mintent = Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"))
-//            btn_5 -> mintent = ""
-//        }
+        btnListner = View.OnClickListener { // 클릭이 되었을 때의 참조값이 it에 들어옴!
+            when(it.id){
+                R.id.buttonNaver -> intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"))
+                R.id.button911 -> intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"))
+                R.id.buttonGallery -> intent = Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"))
+            }
+            startActivity(intent)
+        }
+
+        btnNaver.setOnClickListener(btnListner)
+        btn911.setOnClickListener(btnListner)
+        btnGallery.setOnClickListener(btnListner)
 
         btn.setOnClickListener {
             Toast.makeText(applicationContext, "기분 좋은 오후입니다 :)", Toast.LENGTH_SHORT).show()
         }
-        btnNaver.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com")))
-        }
-        btn911.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911")))
-        }
-        btnGallery.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media")))
-        }
+//        btnNaver.setOnClickListener {
+//            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com")))
+//        }
+//        btn911.setOnClickListener {
+//            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911")))
+//        }
+//        btnGallery.setOnClickListener {
+//            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media")))
+//        }
         btnFinish.setOnClickListener {
             finish()
         }
